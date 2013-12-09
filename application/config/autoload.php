25f2683aad9d857bbac6c1de09674a752f40f7d0
Faust Gertz <faust@faustgertz.com>
2013-12-09 10:30:14 -0600

    Create application config file and add it to autoload.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -80,7 +80,7 @@ $autoload['helper'] = array();
 |
 */
 
-$autoload['config'] = array();
+$autoload['config'] = array('favorite_things_config');
 
 
 /*
---
+++ application/config/favorite_things_config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Place to dump custome application specific variables
+ *
+ * $config['key'] = 'value';
+ */
+$config['site_name'] = 'Favorite Things - Vesterheim';
