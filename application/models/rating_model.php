ad4342eae3fbc4a4502868a8b39912f954a9d849
Faust Gertz <faust@faustgertz.com>
2013-12-15 11:35:14 -0600

    Add update() method and modify add() method to accept optional  param from update() method.

--- application/models/rating_model.php
+++ application/models/rating_model.php
@@ -54,17 +54,18 @@ class Rating_model extends MY_Model
     }  
 
     /**
-      * Add Artifact Rating
-      * Inserts a new row
-      *
-      * @access public
-      * @param int $artifact_id of artifact being rated
-      * @param int $rating between 1 and 10
-      * @param string $ip_address 
-      * @return boolean|int either the rating_id on success 
-      *                     or FALSE on failure.
-      */
-    public function add($artifact_id, $rating, $ip_address) 
+     * Add Artifact Rating
+     * Inserts a new row
+     *
+     * @access public
+     * @param int $artifact_id of artifact being rated
+     * @param int $rating between 1 and 10
+     * @param string $ip_address 
+     * @param int $previous_id (optional) of row this one replaces     
+     * @return boolean|int either the rating_id on success 
+     *                     or FALSE on failure.
+     */
+    public function add($artifact_id, $rating, $ip_address, $previous_id=FALSE) 
     {
         if (is_idish($artifact_id) === FALSE)
         {
@@ -78,16 +79,54 @@ class Rating_model extends MY_Model
         {
             throw new InvalidArgumentException('Rating_model::add() expects a valid IP address ip_address parameter.   Input was: ' . $ip_address);
         }         
-
+        if ($previous_id !== FALSE && is_idish($previous_id) === FALSE)
+        {
+            throw new InvalidArgumentException('Rating_model::add() expects an integer for the previous_id parameter.   Input was: ' . $previous_id);
+        } 
         $this->db->set('artifact_id', clean_id($artifact_id));
         $this->db->set('rating', clean_rating($rating));
         $this->db->set('status', 1);
-        $this->db->set('ip_address', "INET_ATON('$ip_address')", FALSE);  
+        $this->db->set('ip_address', "INET_ATON('$ip_address')", FALSE);
+        if ($previous_id !== FALSE)
+        {
+            $this->db->set('previous_id', clean_id($previous_id));
+        }         
         $this->db->insert($this->table());
         if ($this->db->affected_rows() === 0)
         {
             return FALSE;
         }
         return $this->db->insert_id();
-    }    
+    } 
+
+    /**
+     * Update Artifact Rating
+     * Sets status of rating row being updated to FALSE
+     * and calls add() method to insert new rating.
+     *
+     * @access public
+     * @param int $id (optional) of row being replaced        
+     * @param int $artifact_id of artifact being rated
+     * @param int $rating between 1 and 10
+     * @param string $ip_address  
+     * @return boolean|int either the rating_id on success 
+     *                     or FALSE on failure.
+     */
+    public function update($id, $artifact_id, $rating, $ip_address) 
+    {
+        if (is_idish($id) === FALSE)
+        {
+            throw new InvalidArgumentException('Rating_model::update() expects an integer for the id parameter.   Input was: ' . $id);
+        } 
+        $this->db->set('status', 0);
+        $this->db->where('id', clean_id($id));  
+        $this->db->where('artifact_id', clean_id($artifact_id));    
+        $this->db->update($this->table());
+        if ($this->db->affected_rows() === 0)
+        {
+            return FALSE;
+        }
+        return $this->add($artifact_id, $rating, $ip_address, $id);
+    }
+
 }
